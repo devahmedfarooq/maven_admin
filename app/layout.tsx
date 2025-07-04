ddde5a8059a9ab9dc45a58fd8b30e270f38c2c52
devahmedfarooq <ahmedfarooq.dev@gmail.com>
2025-07-04 22:07:45 +0500

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import "./globals.css";
 import { AntdRegistry } from '@ant-design/nextjs-registry';
 import Providers from "./provider";
@@ -11,10 +11,7 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -31,7 +28,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}  antialiased`}
+        className={`${geistSans.variable}  antialiased`}
       >
         <Providers><AntdRegistry>{children}</AntdRegistry></Providers>
       </body>
